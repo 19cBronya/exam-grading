54ae617d36b1df6ca55fa6d62b555f87b36bfddc
19cBronya <wxj2003@outlook.com>
2025-01-11 02:19:37 +0800

    add rustlings

---
+++ rustlings/exercises/variables/variables5.rs
@@ -0,0 +1,13 @@
+// variables5.rs
+//
+// Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    let number = 3; // don't rename this variable
+    let mut number = 3; //() don't rename this variable// no matter mut or immutable ,let could Shadowing it all
+    println!("Number plus two is : {}", number + 2);
+}
